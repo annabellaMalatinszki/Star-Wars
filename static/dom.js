7dcab84649c9d1c9e60a5ee65743707c0edeb0f2
Annabella Malatinszki <a.malatinszki@gmail.com>
2017-07-02 19:11:44 +0200

    Create dom.js for dom manipulation and adding table data

---
+++ static/dom.js
@@ -0,0 +1,7 @@
+var app = app || {};
+
+app.dom = {
+    displayPlanets: function () {
+
+    }
+}
--- static/main.js
+++ static/main.js
@@ -2,6 +2,7 @@ var app = app || {};
 
 app.init = function () {
     app.dataManager.getPlanets()
+    app.dom.displayPlanets()
 };
 
 app.init();
