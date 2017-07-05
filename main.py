44d5770b472aa61aecd7d4b67fca4f54d87b0939
Annabella Malatinszki <a.malatinszki@gmail.com>
2017-07-05 17:25:14 +0200

    Implement basics of user account handling in main.py

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
-from flask import Flask, url_for, render_template, redirect, request
-
+from flask import Flask, url_for, render_template, redirect, request, session, flash, g
+import os
 
 app = Flask(__name__)
+app.secret_key = os.urandom(24)
 
 
 @app.route("/")
-def show_main_page():
+def index():
     return render_template("index.html")
 
 
@@ -14,9 +15,30 @@ def register():
     return render_template("register.html")
 
 
-@app.route("/login")
+@app.route("/login", methods=["GET", "POST"])
 def login():
+    elif request.method == "POST":
+        session.pop("user", None)
+        if request.form["pwd"] == "admin":
+            # a function checking the password belonging to the user in the database
+            # should go here with the user_name and password as arguments
+            session["user"] = request.form["user_name"]
+            # logged in stuff should come here
     return render_template("login.html")
+
+
+@app.route("/logout")
+def logout():
+    session.pop("user", None)
+    return "Logged out"
+
+
+@app.before_request
+def before_request():
+    g.user = None
+    if "user" in session:
+        g.user = session["user"]
+# to make something password protected, you have to envelop it in "if g.user:"
 
 
 if __name__ == "__main__":
