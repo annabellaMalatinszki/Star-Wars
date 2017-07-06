482fa6b4b458c5079fee7b20cadaefa051ec10de
Annabella Malatinszki <a.malatinszki@gmail.com>
2017-07-06 13:30:35 +0200

    Create planet_votes table in database

--- star_wars.sql
+++ star_wars.sql
@@ -1,4 +1,6 @@
 ALTER TABLE IF EXISTS ONLY public.accounts DROP CONSTRAINT IF EXISTS pk_accounts_id CASCADE;
+ALTER TABLE IF EXISTS ONLY public.planet_votes DROP CONSTRAINT IF EXISTS pk_planet_votes_id CASCADE;
+
 
 DROP TABLE IF EXISTS public.accounts;
 DROP SEQUENCE IF EXISTS public.accounts_id_seq;
@@ -12,3 +14,20 @@ CREATE TABLE accounts (
 ALTER TABLE ONLY accounts
     ADD CONSTRAINT pk_accounts_id PRIMARY KEY (id);
 
+
+DROP TABLE IF EXISTS public.planet_votes;
+DROP SEQUENCE IF EXISTS public.planet_votes_id_seq;
+CREATE TABLE planet_votes (
+    id serial NOT NULL,
+    planet_id int NOT NULL,
+    planet_name varchar NOT NULL,
+    user_id int NOT NULL,
+    submission_time timestamp without time zone NOT NULL
+);
+
+ALTER TABLE ONLY planet_votes
+    ADD CONSTRAINT pk_planet_votes_id PRIMARY KEY (id);
+
+ALTER TABLE ONLY planet_votes
+    ADD CONSTRAINT fk_user_id FOREIGN KEY (user_id) REFERENCES accounts(id)
+    ON UPDATE CASCADE ON DELETE CASCADE;
